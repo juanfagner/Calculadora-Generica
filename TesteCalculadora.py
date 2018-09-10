3e61cc83bc655e604a1b5da81bbd568b2e544502
Juan Garcia <juanfagner@gmail.com>
2018-09-10 18:06:27 -0300

    Testes

--- TesteCalculadora.py
+++ TesteCalculadora.py
@@ -84,21 +84,48 @@ class TesteCalculadora():
         return 0
 
 
+if  __name__ == "__main__":
+    print("Testes")
+    teste = TesteCalculadora
+
+    print("Teste de Soma:")
+    teste.test_CalcularSoma(10, 2,3,5)
+    teste.test_CalcularSoma(10, 1,3,5)
+    print("================================")
+    print("Teste de Media:")
+
+    teste.test_CalcularMedia(3, 1, 6, 3, 4, 5)
+    teste.test_CalcularMedia(3, 1, 2, 3, 4, 5)
+
+    print("================================")
+    print("Teste de Binario:")
+
+    teste.test_getBinario(10, 2)
+    teste.test_getBinario(1011, 25)
+
+    print("================================")
+    print("Teste de Numero de Digitos:")
+
+    teste.test_getNumerodeDigitos(5, 12345)
+    teste.test_getNumerodeDigitos(3, 4123)
+
+    print("================================")
+    print("Teste de Numero Primo:")
+
+    teste.test_ePrimo(11, 7)
+    teste.test_ePrimo(227, 227)
+
+    print("================================")
+    print("Teste de Palidromo:")
+
+    teste.test_ePalidromo(7217, 7137)
+    teste.test_ePalidromo(7117, 7117)
 
 
-teste = TesteCalculadora
-teste.test_ePalidromo(7117, 7117)
-teste.test_ePalidromo(7127, 7117)
-teste.test_ePrimo(7, 7)
-teste.test_ePrimo(7, 11)
-teste.test_getNumerodeDigitos(5, 12345)
-teste.test_getNumerodeDigitos(3, 4123)
-teste.test_getBinario(10, 2)
-teste.test_getBinario(1011, 25)
-teste.test_CalcularMedia(3, 1,2,3,4,5)
-teste.test_CalcularMedia(3, 1,1,3,4,5)
-teste.test_CalcularSoma(10, 2,3,5)
-teste.test_CalcularSoma(10, 1,3,5)
+
+
+
+
 
 
 
